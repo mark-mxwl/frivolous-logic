f36b9e4c6ddffe79b391bf643196fa07b9950950
mark-mxwl <138938022+mark-mxwl@users.noreply.github.com>
2023-09-19 20:21:03 -0700

    Slight refactor. Added button toggle.

--- TRTBA_v1/src/components/Puzzle1.jsx
+++ TRTBA_v1/src/components/Puzzle1.jsx
@@ -2,25 +2,38 @@ import { useState, useEffect } from 'react'
 
 export default function Puzzle1(){
     const colors = ["gold", "white", "gray", "pink", "turquoise"]
-    const randomColors = colors.sort(() => .5 - Math.random()).slice(0, 4)
     const [slotColor, setSlotColor] = useState([])
+    const [toggle, setToggle] = useState(false)
     
     useEffect(() => {
+        if(toggle === true){
         const interval = setInterval(() => {
-            setSlotColor(randomColors)}, 1000)
+            setSlotColor(getRandomColors)}, 1000)
         return () => clearInterval(interval)
-    }, [randomColors])
-    
+        }
+    }, [getRandomColors])
+
+    function handleStartClick(e){
+        setToggle(true)
+        getRandomColors()
+    }
+    function getRandomColors() {
+        return colors.sort(() => .5 - Math.random()).slice(0, 4)
+    }
+
     function handleSlotClick(e){
         console.log(e)
     }
 
     return (
-        <div className="diamond-container">
-            <div id={slotColor[0]} className="diamond" onClick={handleSlotClick}></div>
-            <div id={slotColor[1]} className="diamond" onClick={handleSlotClick}></div>
-            <div id={slotColor[2]} className="diamond" onClick={handleSlotClick}></div>
-            <div id={slotColor[3]} className="diamond" onClick={handleSlotClick}></div>
+        <div className='puzzle-layout'>
+            <button id="start-btn" onClick={handleStartClick}>Start Game</button><br />
+            <div className="diamond-container">
+                <div id={slotColor[0]} className="diamond" onClick={handleSlotClick}></div>
+                <div id={slotColor[1]} className="diamond" onClick={handleSlotClick}></div>
+                <div id={slotColor[2]} className="diamond" onClick={handleSlotClick}></div>
+                <div id={slotColor[3]} className="diamond" onClick={handleSlotClick}></div>
+            </div>
         </div>
     )
 }
