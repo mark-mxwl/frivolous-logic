cf04732719c722299855257b8d035648faf5a086
mark-mxwl <138938022+mark-mxwl@users.noreply.github.com>
2023-09-21 19:56:22 -0700

    Refactored puzz1 slots; added props/styles/state

--- TRTBA_v1/src/components/Puzzle1.jsx
+++ TRTBA_v1/src/components/Puzzle1.jsx
@@ -1,8 +1,36 @@
 import { useState, useEffect } from 'react'
+import Slot from './Slot'
 
 export default function Puzzle1(){
-    const colors = ["gold", "white", "gray", "pink", "turquoise"]
+    // this arr is randomly SORTED by getRandomColors every second (via useEffects)
+    const colors = [
+        {
+            value: "gold",
+            isHeld: false
+        }, 
+        {
+            value: "white",
+            isHeld: false
+        }, 
+        {
+            value: "gray",
+            isHeld: false
+        }, 
+        {
+            value: "pink",
+            isHeld: false
+        }, 
+        {
+            value: "turquoise",
+            isHeld: false
+        }
+    ]
+    //this arr is SET with getRandomColors via useEffect
     const [slotColor, setSlotColor] = useState([])
+
+    //this arr is POPULATED by handleSlotClick and has a max length of 4 items (to determine winner)
+    const [selectedColors, setSelectedColors] = useState([])
+    
     const [toggle, setToggle] = useState(false)
     
     useEffect(() => {
@@ -17,22 +45,52 @@ export default function Puzzle1(){
         setToggle(true)
         getRandomColors()
     }
+
     function getRandomColors() {
-        return colors.sort(() => .5 - Math.random()).slice(0, 4)
+        // randomly sorts colors arr, then returns a sliced arr with 4 values [0, 1, 2, 3]
+        //          0
+        //      2       1
+        //          3
+        return colors.toSorted(() => .5 - Math.random()).slice(0, 4)
+        
     }
 
     function handleSlotClick(e){
-        console.log(e)
+        setSelectedColors(prev => [...prev, {value: `${e.target.id}`, isHeld: true}].slice(0, 4))
+        console.log(selectedColors)
+        console.log(e.target.id)
+
     }
 
     return (
         <div className='puzzle-layout'>
             <button id="start-btn" onClick={handleStartClick}>Start Game</button><br />
             <div className="diamond-container">
-                <div id={slotColor[0]} className="diamond" onClick={handleSlotClick}></div>
-                <div id={slotColor[1]} className="diamond" onClick={handleSlotClick}></div>
-                <div id={slotColor[2]} className="diamond" onClick={handleSlotClick}></div>
-                <div id={slotColor[3]} className="diamond" onClick={handleSlotClick}></div>
+                {/* arr indexes in Slots for isHeld/selectedColor need correct assignment to selected slot */}
+                <Slot 
+                    slotColor={slotColor[0]?.value}
+                    isHeld={selectedColors[0]?.isHeld}
+                    selectedColor={selectedColors[0]?.value} 
+                    handleSlotClick={handleSlotClick}
+                />
+                <Slot 
+                    slotColor={slotColor[1]?.value}
+                    isHeld={selectedColors[1]?.isHeld} 
+                    selectedColor={selectedColors[1]?.value} 
+                    handleSlotClick={handleSlotClick}
+                />
+                <Slot 
+                    slotColor={slotColor[2]?.value}
+                    isHeld={selectedColors[2]?.isHeld} 
+                    selectedColor={selectedColors[2]?.value} 
+                    handleSlotClick={handleSlotClick}
+                />
+                <Slot 
+                    slotColor={slotColor[3]?.value}
+                    isHeld={selectedColors[3]?.isHeld}
+                    selectedColor={selectedColors[3]?.value} 
+                    handleSlotClick={handleSlotClick} 
+                />
             </div>
         </div>
     )
